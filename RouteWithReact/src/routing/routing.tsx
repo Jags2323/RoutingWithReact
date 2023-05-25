bc0399f6b53d9d0fbd20e6793a5ee01310c83657
JagsP23 <pullapuramjr@gmail.com>
2023-05-25 16:00:00 -0400

    Layout routes

--- RouteWithReact/src/routing/routing.tsx
+++ RouteWithReact/src/routing/routing.tsx
@@ -5,6 +5,7 @@ import UserDetails from "../components/UserDetails";
 import UsersPage from "../components/UsersPage";
 import ErrorPage from "../components/ErrorPage";
 import LoginPage from "../components/LoginPage";
+import PrivateRoutes from "../components/PrivateRoutes";
 
 // array of route objects
 // two properties path and element to render in that path
@@ -13,12 +14,18 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     // handle error page
-    errorElement:<ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "/login", element: <LoginPage /> },
       //   { path: "/users", element: <ListPage /> },
       //   { path: "/contact", element: <ContactPage /> },
+    ],
+  },
+  // Layout Route
+  {
+    element: <PrivateRoutes />,
+    children: [
       {
         path: "users",
         element: <UsersPage />,
